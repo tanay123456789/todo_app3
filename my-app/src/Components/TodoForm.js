c5e94f07e6db755b3d931b79e61cf87ea6318327
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2021-08-04 08:49:01 +0530

    feature:added form

---
+++ my-app/src/Components/TodoForm.js
@@ -0,0 +1,43 @@
+import React ,{useState} from "react"
+
+import {FormGroup,Input,InputGroup,InputGroupAddon,Button,Form} from "reactstrap";
+
+import {v4} from "uuid"
+
+const TodoForm=({addTodos}) =>{
+    const[todoString,setTodoString]=useState("");
+
+    const handleSubmit=e=>{
+        e.preventDefault();
+        if(todoString===""){
+            return alert("Please fill some value");
+        }
+        const todo={
+            todoString,
+            id:v4()
+        };
+        addTodos(todo);
+        setTodoString("");
+    }
+
+
+    return(
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                <InputGroup>
+                    <Input type="text" 
+                    name="todo"
+                    id="todo"
+                    placeholder="Enter a todoString"
+                    value={todoString}
+                    onChange={e=>setTodoString(e.target.value)}/>
+                
+                <InputGroupAddon addonType="prepend">
+                <Button color="success">addTodo</Button>
+
+                </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+}
